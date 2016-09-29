7df4576e06c7dc5896b00e99babbc5021f734166
BlueBug <iopzxz@gmail.com>
2016-09-29 10:29:33 -0400

    UI context is created
    it is the beginning small step but I now know how imgui works so Ithink
    I can make this class

---
+++ DX11Starter_copy/Files/UI/UIContext.cpp
@@ -0,0 +1,5 @@
+#include "UIContext.h"
+using namespace KUI;
+UIContext::UIContext(void* hwnd, ID3D11Device* device, ID3D11DeviceContext* device_context) {
+
+}
--- DX11Starter_copy/Files/UI/UIContext.h
+++ DX11Starter_copy/Files/UI/UIContext.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "imgui_impl_dx11.h"
+/**
+	Using imgi dx11 implementation, and leaving the DXCore and window initialization work needed to others.
+**/
+namespace KUI {
+
+	class UIContext {
+	public:
+		UIContext(void* hwnd, ID3D11Device* device, ID3D11DeviceContext* device_context);
+
+	};
+}
